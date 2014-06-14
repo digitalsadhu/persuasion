3457885b8b01d4e971d13853d83a5ad4ee7de4d5
Richard Walker <digitalsadhu@gmail.com>
2014-06-14 20:48:27 +0200

    Add tests and code for module

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 'use strict';
 
-module.exports = function () {
+module.exports = function (arr, options) {
+
+  var opts = options || {}
+
+  var processAsObject = true;
+  for (var i in arr) {
+    if (arr.hasOwnProperty(i)) {
+      if (typeof arr[i] !== 'object') {
+        processAsObject = false
+        break;
+      }
+    }
+  }
+
+  //sort objects
+  if (processAsObject) {
+    if (typeof opts.property === 'undefined')
+      throw new Error("when sorting objects, 'property' must be set in options")
+    return arr.sort(function (left, right) {
+      if (left[opts.property] > right[opts.property]) return (opts.reverse) ? -1 : 1
+      if (right[opts.property] > left[opts.property]) return (opts.reverse) ? 1 : -1
+      if (left[opts.property] === right[opts.property]) return 0
+    })
+
+  }
+
+  //sort strings and integers
+  return arr.sort(function (left, right) {
+    if (left > right) return (opts.reverse) ? -1 : 1
+    if (right > left) return (opts.reverse) ? 1 : -1
+    if (left === right) return 0
+  })
 
 }
--- test/persuasion.spec.js
+++ test/persuasion.spec.js
@@ -1,7 +1,95 @@
 var expect = require('chai').expect
+  , sort   = require('../index.js')
 
-describe('', function () {
-  it('', function () {
-
+describe('persuasion', function () {
+  describe('string sorting', function () {
+    it('should accept an array of strings and return it sorted', function () {
+      var data = ['zebra','monkey','horse','cow']
+      var sorted = sort(data)
+      expect(sorted).to.be.an('array')
+      expect(sorted[0]).to.equal('cow')
+      expect(sorted[1]).to.equal('horse')
+      expect(sorted[2]).to.equal('monkey')
+      expect(sorted[3]).to.equal('zebra')
+    })
+  })
+  describe('string sorting in reverse', function () {
+    it('should accept an array of strings and return it sorted', function () {
+      var data = ['zebra','monkey','horse','cow']
+      var sorted = sort(data, { reverse: true })
+      expect(sorted).to.be.an('array')
+      expect(sorted[0]).to.equal('zebra')
+      expect(sorted[1]).to.equal('monkey')
+      expect(sorted[2]).to.equal('horse')
+      expect(sorted[3]).to.equal('cow')
+    })
+  })
+  describe('integer sorting', function () {
+    it('should accept an array of ints and return them sorted', function () {
+      var data = [12,1,10,3]
+      var sorted = sort(data)
+      expect(sorted).to.be.an('array')
+      expect(sorted[0]).to.equal(1)
+      expect(sorted[1]).to.equal(3)
+      expect(sorted[2]).to.equal(10)
+      expect(sorted[3]).to.equal(12)
+    })
+  })
+  describe('integer sorting in reverse', function () {
+    it('should force sorting to be in descending order', function () {
+      var data = [12,1,10,3]
+      var sorted = sort(data, {reverse: true})
+      expect(sorted).to.be.an('array')
+      expect(sorted[0]).to.equal(12)
+      expect(sorted[1]).to.equal(10)
+      expect(sorted[2]).to.equal(3)
+      expect(sorted[3]).to.equal(1)
+    })
+  })
+  describe('object sorting', function () {
+    it('should accept an array of objects and return them sorted as specified', function () {
+      var data = [
+        { name: 'tom' },
+        { name: 'bob' },
+        { name: 'andrew' },
+        { name: 'simon' }
+      ]
+      var sorted = sort(data, { property: 'name' })
+      expect(sorted).to.be.an('array')
+      expect(sorted[0].name).to.equal('andrew')
+      expect(sorted[1].name).to.equal('bob')
+      expect(sorted[2].name).to.equal('simon')
+      expect(sorted[3].name).to.equal('tom')
+    })
+    it('should throw an error if property is not specified', function () {
+      error = false
+      var data = [
+        { name: 'tom' },
+        { name: 'bob' }
+      ]
+      try {
+        var sorted = sort(data)
+      } catch (err) {
+        error = err
+      }
+      expect(error instanceof Error).to.equal(true)
+      expect(error.message).to.equal("when sorting objects, 'property' must be set in options")
+    })
+  })
+  describe('object sorting in reverse', function () {
+    it('should accept an array of objects and return them sorted as specified in reverse', function () {
+      var data = [
+        { name: 'tom' },
+        { name: 'bob' },
+        { name: 'andrew' },
+        { name: 'simon' }
+      ]
+      var sorted = sort(data, { property: 'name', reverse: true })
+      expect(sorted).to.be.an('array')
+      expect(sorted[0].name).to.equal('tom')
+      expect(sorted[1].name).to.equal('simon')
+      expect(sorted[2].name).to.equal('bob')
+      expect(sorted[3].name).to.equal('andrew')
+    })
   })
 })
